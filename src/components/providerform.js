aab84423c943164381d5d8ad4f51c4d30e13649b
vsai2314108 <somasundaramv@ufl.edu>
2022-11-05 17:43:46 -0400

    fix provider form

--- src/components/providerform.js
+++ src/components/providerform.js
@@ -1,54 +1,72 @@
 import { Container, FormGroup } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import Header from 'react-bootstrap/Navbar';
 
 function ProviderForm() {
     //Age, Gender, ScheudleDay-Time, AppointmentDay, Diabetes, Hypertesion, Welfare
 
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        let age = e.target[0].value;
+        let gender = e.target[1].value;
+        let alcholic = e.target[2].value;
+        let diabetes = e.target[3].value;
+        let hypertension = e.target[4].value;
+        let welfare = e.target[5].value;
+        console.log(age,gender,alcholic,diabetes,hypertension,welfare)
+    }
     return (
         <Container>
-            <Header>Patient Form</Header>
-            <Form>
-                <FormGroup>
+            <Header >Patient Form</Header>
+            <Form onSubmit={onFormSubmit}>
+                <FormGroup className="mb-3">
                     <Form.Label>Age</Form.Label>
                     <Form.Control type="number" placeholder="Enter Age" />
                 </FormGroup>
-                <FormGroup>
+                <FormGroup className="mb-3">
                     <Form.Label>Gender</Form.Label>
                     <Form.Select>
+                        <option value="1">Choose not to identify</option>
                         <option value="0">Male</option>
                         <option value="1">Female</option>
-                        <option value="1">Choose not to identify</option>
                     </Form.Select>
-                </FormGroup>
-                <FormGroup>
+                </FormGroup >
+                <FormGroup className="mb-3">
                     <Form.Label>Alcholic</Form.Label>
                     <Form.Select>
+                        <option value="1">Choose not to answer</option>
                         <option value="0">No</option>
                         <option value="1">Yes</option>
                     </Form.Select>
                 </FormGroup>
-                <FormGroup>
+                <FormGroup className="mb-3">
                     <Form.Label>Diabetes</Form.Label>
                     <Form.Select>
+                        <option value="1">Choose not to answer</option>
                         <option value="0">No</option>
                         <option value="1">Yes</option>
                     </Form.Select> 
-                </FormGroup>
-                <FormGroup>
+                </FormGroup >
+                <FormGroup className="mb-3">
                     <Form.Label>Hypertension</Form.Label>
                     <Form.Select>
+                        <option value="1">Choose not to answer</option>
                         <option value="0">No</option>
                         <option value="1">Yes</option>
                     </Form.Select>
                 </FormGroup>
-                <FormGroup>
+                <FormGroup className="mb-3">
                     <Form.Label>Welfare</Form.Label>
                     <Form.Select>
+                        <option value="1">Choose not to answer</option>
                         <option value="0">No</option>
                         <option value="1">Yes</option>
                     </Form.Select>
                 </FormGroup>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
             </Form>
         </Container>
     )
