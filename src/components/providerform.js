abef488737ee4faa7657169c2d3b19eafe657ea2
vsai2314108 <somasundaramv@ufl.edu>
2022-11-05 17:57:38 -0400

    fixed list

--- src/components/providerform.js
+++ src/components/providerform.js
@@ -8,13 +8,15 @@ function ProviderForm() {
 
     const onFormSubmit = (e) => {
         e.preventDefault();
-        let age = e.target[0].value;
-        let gender = e.target[1].value;
-        let alcholic = e.target[2].value;
-        let diabetes = e.target[3].value;
-        let hypertension = e.target[4].value;
-        let welfare = e.target[5].value;
-        console.log(age,gender,alcholic,diabetes,hypertension,welfare)
+        let age = Math.min(e.target[0].value/100.00, 1);
+        let gender = parseInt(e.target[1].value);
+        let alcholic = parseInt(e.target[2].value);
+        let diabetes = parseInt(e.target[3].value);
+        let hypertension = parseInt(e.target[4].value);
+        let welfare = parseInt(e.target[5].value);
+        let handicap = parseInt(e.target[6].value);
+        let sms = parseInt(e.target[7].value);
+        console.log(gender, age, welfare, hypertension, diabetes, alcholic, handicap, sms)
     }
     return (
         <Container>
@@ -22,14 +24,14 @@ function ProviderForm() {
             <Form onSubmit={onFormSubmit}>
                 <FormGroup className="mb-3">
                     <Form.Label>Age</Form.Label>
-                    <Form.Control type="number" placeholder="Enter Age" />
+                    <Form.Control type="number" placeholder="Enter Age" defaultValue={50} />
                 </FormGroup>
                 <FormGroup className="mb-3">
                     <Form.Label>Gender</Form.Label>
                     <Form.Select>
-                        <option value="1">Choose not to identify</option>
-                        <option value="0">Male</option>
-                        <option value="1">Female</option>
+                        <option value="0">Choose not to identify</option>
+                        <option value="1">Male</option>
+                        <option value="0">Female</option>
                     </Form.Select>
                 </FormGroup >
                 <FormGroup className="mb-3">
@@ -60,6 +62,21 @@ function ProviderForm() {
                     <Form.Label>Welfare</Form.Label>
                     <Form.Select>
                         <option value="1">Choose not to answer</option>
+                        <option value="0">No</option>
+                        <option value="1">Yes</option>
+                    </Form.Select>
+                </FormGroup>
+                <FormGroup className="mb-3">
+                    <Form.Label>Handicap</Form.Label>
+                    <Form.Select>
+                        <option value="1">Choose not to answer</option>
+                        <option value="0">No</option>
+                        <option value="1">Yes</option>
+                    </Form.Select>
+                </FormGroup>
+                <FormGroup className="mb-3">
+                    <Form.Label>SMS Sent</Form.Label>
+                    <Form.Select>
                         <option value="0">No</option>
                         <option value="1">Yes</option>
                     </Form.Select>
