861b329892133946370b4ad89de2a45e16964707
vsai2314108 <somasundaramv@ufl.edu>
2022-11-05 21:30:14 -0400

    adding backedn

--- src/components/providerform.js
+++ src/components/providerform.js
@@ -2,9 +2,12 @@ import { Container, FormGroup } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Header from 'react-bootstrap/Navbar';
+import { useState } from 'react';
 
 function ProviderForm() {
-    //Age, Gender, ScheudleDay-Time, AppointmentDay, Diabetes, Hypertesion, Welfare
+    //Age, Gender, ScheudleDay-Time, AppointmentDay, Diabetes, Hypertesion, Welfare, Handicap, SMS
+    const [out, setOut] = useState([]);
+    const [blank, setBlank] = useState(true);
 
     const onFormSubmit = (e) => {
         e.preventDefault();
@@ -16,77 +19,94 @@ function ProviderForm() {
         let welfare = parseInt(e.target[5].value);
         let handicap = parseInt(e.target[6].value);
         let sms = parseInt(e.target[7].value);
-        console.log(gender, age, welfare, hypertension, diabetes, alcholic, handicap, sms)
+        let data = [[gender, age, welfare, hypertension, diabetes, alcholic, handicap, sms]]
+        fetch("http://localhost:8080/predict", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        }).then((response) => response.json())
+        .then((data) => {
+            console.log(data); 
+            setOut(data);
+        }).then(setBlank(false));
     }
-    return (
-        <Container>
-            <Header >Patient Form</Header>
-            <Form onSubmit={onFormSubmit}>
-                <FormGroup className="mb-3">
-                    <Form.Label>Age</Form.Label>
-                    <Form.Control type="number" placeholder="Enter Age" defaultValue={50} />
-                </FormGroup>
-                <FormGroup className="mb-3">
-                    <Form.Label>Gender</Form.Label>
-                    <Form.Select>
-                        <option value="0">Choose not to identify</option>
-                        <option value="1">Male</option>
-                        <option value="0">Female</option>
-                    </Form.Select>
-                </FormGroup >
-                <FormGroup className="mb-3">
-                    <Form.Label>Alcholic</Form.Label>
-                    <Form.Select>
-                        <option value="1">Choose not to answer</option>
-                        <option value="0">No</option>
-                        <option value="1">Yes</option>
-                    </Form.Select>
-                </FormGroup>
-                <FormGroup className="mb-3">
-                    <Form.Label>Diabetes</Form.Label>
-                    <Form.Select>
-                        <option value="1">Choose not to answer</option>
-                        <option value="0">No</option>
-                        <option value="1">Yes</option>
-                    </Form.Select> 
-                </FormGroup >
-                <FormGroup className="mb-3">
-                    <Form.Label>Hypertension</Form.Label>
-                    <Form.Select>
-                        <option value="1">Choose not to answer</option>
-                        <option value="0">No</option>
-                        <option value="1">Yes</option>
-                    </Form.Select>
-                </FormGroup>
-                <FormGroup className="mb-3">
-                    <Form.Label>Welfare</Form.Label>
-                    <Form.Select>
-                        <option value="1">Choose not to answer</option>
-                        <option value="0">No</option>
-                        <option value="1">Yes</option>
-                    </Form.Select>
-                </FormGroup>
-                <FormGroup className="mb-3">
-                    <Form.Label>Handicap</Form.Label>
-                    <Form.Select>
-                        <option value="1">Choose not to answer</option>
-                        <option value="0">No</option>
-                        <option value="1">Yes</option>
-                    </Form.Select>
-                </FormGroup>
-                <FormGroup className="mb-3">
-                    <Form.Label>SMS Sent</Form.Label>
-                    <Form.Select>
-                        <option value="0">No</option>
-                        <option value="1">Yes</option>
-                    </Form.Select>
-                </FormGroup>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </Container>
-    )
+    if (blank) {
+        return (
+            <Container>
+                <Header >Patient Form</Header>
+                <Form onSubmit={onFormSubmit}>
+                    <FormGroup className="mb-3">
+                        <Form.Label>Age</Form.Label>
+                        <Form.Control type="number" placeholder="Enter Age" defaultValue={50} />
+                    </FormGroup>
+                    <FormGroup className="mb-3">
+                        <Form.Label>Gender</Form.Label>
+                        <Form.Select>
+                            <option value="0">Choose not to identify</option>
+                            <option value="1">Male</option>
+                            <option value="0">Female</option>
+                        </Form.Select>
+                    </FormGroup >
+                    <FormGroup className="mb-3">
+                        <Form.Label>Alcholic</Form.Label>
+                        <Form.Select>
+                            <option value="1">Choose not to answer</option>
+                            <option value="0">No</option>
+                            <option value="1">Yes</option>
+                        </Form.Select>
+                    </FormGroup>
+                    <FormGroup className="mb-3">
+                        <Form.Label>Diabetes</Form.Label>
+                        <Form.Select>
+                            <option value="1">Choose not to answer</option>
+                            <option value="0">No</option>
+                            <option value="1">Yes</option>
+                        </Form.Select> 
+                    </FormGroup >
+                    <FormGroup className="mb-3">
+                        <Form.Label>Hypertension</Form.Label>
+                        <Form.Select>
+                            <option value="1">Choose not to answer</option>
+                            <option value="0">No</option>
+                            <option value="1">Yes</option>
+                        </Form.Select>
+                    </FormGroup>
+                    <FormGroup className="mb-3">
+                        <Form.Label>Welfare</Form.Label>
+                        <Form.Select>
+                            <option value="1">Choose not to answer</option>
+                            <option value="0">No</option>
+                            <option value="1">Yes</option>
+                        </Form.Select>
+                    </FormGroup>
+                    <FormGroup className="mb-3">
+                        <Form.Label>Handicap</Form.Label>
+                        <Form.Select>
+                            <option value="1">Choose not to answer</option>
+                            <option value="0">No</option>
+                            <option value="1">Yes</option>
+                        </Form.Select>
+                    </FormGroup>
+                    <FormGroup className="mb-3">
+                        <Form.Label>SMS Sent</Form.Label>
+                        <Form.Select>
+                            <option value="0">No</option>
+                            <option value="1">Yes</option>
+                        </Form.Select>
+                    </FormGroup>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
+        )
+    }
+    else
+    {
+        return (
+            <p> This Patient has a {out} </p>
+        )
+    }
 }
 
 export default ProviderForm;
